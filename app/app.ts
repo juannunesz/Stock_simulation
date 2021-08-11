fe114ec5aca343d104986a2348fbcbcce7f8ed42
juannunesz <juannuneszbr@gmail.com>
2021-08-11 17:36:42 -0300

    first commit

---
+++ app/app.ts
@@ -0,0 +1,10 @@
+import { NegociacaoController } from "./controllers/negociacaoController.js";
+
+const controller = new NegociacaoController();
+
+const from = document.querySelector('.form')
+
+from.addEventListener('submit', event => {
+    event.preventDefault()
+    controller.adiciona()
+})
